820d56c8223c1fec4a2acb64ef2587f011b80cb5
PREKSHA RAKHECHA <preksharakhecha@PREKSHAs-MacBook-Pro.local>
2021-07-03 15:07:03 +0545

    port  fixed  in script.js

--- public/script.js
+++ public/script.js
@@ -5,28 +5,28 @@ myVideo.muted = true;
 var peer = new Peer(undefined, {
     path: "/peerjs",
     host: "/",
-    port: "3030",
+    port: "",
 });
 
 let myVideoStream;
 navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-})
-.then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
-    peer.on("call", (call) => {
-        call.answer(stream);
-        const video = document.createElement("video");
-        call.on("stream", (userVideoStream) => {
-            addVideoStream(video, userVideoStream);
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream);
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
         });
     });
-    socket.on("user-connected", (userId) => {
-        connectToNewUser(userId, stream);
-    });
-});
 
 const connectToNewUser = (userId, stream) => {
     const call = peer.call(userId, stream);
@@ -43,7 +43,7 @@ peer.on("open", (id) => {
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () => {
-       video.play();
+        video.play();
     });
     videoGrid.append(video);
 };
